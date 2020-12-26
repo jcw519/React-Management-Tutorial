b42c0dcb7e223bcf514fdad3470b005034d883b0
jcw519 <chaewonjeong.dev@gmail.com>
2020-12-26 15:54:40 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,85 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '961222',
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '정채원',
+  'birthday': '990519',
   'gender': '남자',
   'job': '대학생'
-}
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이순신',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '디자이너'
+      },
+
+];
 
 class App extends Component {
   render(){
     return (
-      <Customer
-        name = {customer.name}
-        birthday = {customer.birthday}
-        gender = {customer.gender}
-        job = {customer.job}
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+
+
+
+
+        {/*
+        <Customer
+        id= {customers[0].id}
+        image ={customers[0].image}
+        name = {customers[0].name}
+        birthday = {customers[0].birthday}
+        gender = {customers[0].gender}
+        job = {customers[0].job}
       />
+      <Customer
+        id= {customers[1].id}
+        image ={customers[1].image}
+        name = {customers[1].name}
+        birthday = {customers[1].birthday}
+        gender = {customers[1].gender}
+        job = {customers[1].job}
+      />
+      <Customer
+        id= {customers[2].id}
+        image ={customers[2].image}
+        name = {customers[2].name}
+        birthday = {customers[2].birthday}
+        gender = {customers[2].gender}
+        job = {customers[2].job}
+      />
+      */}
+      </div>
+      
     );
   }
 }
