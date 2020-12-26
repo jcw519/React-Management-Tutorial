714404990a6f768d1ba97ba96f7cf1d9a412e2fe
jcw519 <chaewonjeong.dev@gmail.com>
2020-12-26 18:44:40 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'hello express!'})
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '정채원',
+        'birthday': '990519',
+        'gender': '남자',
+        'job': '대학생'
+        },
+        {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '홍길동',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '대학생'
+        },
+        {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '이순신',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '디자이너'
+        }
+    ])
 });
 
 
